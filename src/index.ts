2f9da7f71786264dc84681c787ee6aa601bb9fda
Arthur Adonai <tutuadonai@gmail.com>
2025-12-15 01:33:48 -0300

    feat: expose public api exports

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+export { ZodArguments } from "~/Core/ZodArguments";
+export { ZodFunction } from "~/Function/ZodFunction";
+export { ZodOverloadedFunction } from "~/OverloadedFunction/ZodOverloadedFunction";
+
+export type { ZodIssue } from "~/Common/ZodIssue";
+export { ArgumentsError } from "~/Core/Error/ArgumentsError";
+export { OverloadsError } from "~/OverloadedFunction/Error/OverloadsError";
+
+export type { ZodSchema } from "~/Common/ZodSchema";
+export type { ZodInputFunction } from "~/Common/ZodInputFunction";
+export type { ZodOutputFunction } from "~/Function/Types/ZodOutputFunction";
+
+export type { ZodSchemaDefinition } from "~/Common/ZodSchemaDefinition";
+export type { ZodInputArguments } from "~/Core/Types/ZodInputArguments";
+export type { ZodOutputArguments } from "~/Core/Types/ZodOutputArguments";
+
+export type { ZodOverloadedArguments } from "~/OverloadedFunction/Types/ZodOverloadedArguments";
+export type { ZodOverloadReturn } from "~/OverloadedFunction/Types/ZodOverloadReturn";
